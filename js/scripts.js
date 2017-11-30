d8101fe0eacb4a0c78376a85c50894462b4817c9
Byron Chang and Kate Trahan <kate@>
2017-11-30 11:23:14 -0800

    add html, css, js, images, functions

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("form#dating").submit(function(event) {
+    event.preventDefault();
+    var preference = $("#interest").val();
+    var agerange = parseInt($("input#age").val());
+
+    if (agerange < 21) {
+        $("#yourdate").addClass("puppydate");
+        $("#date").text("a puppy");
+    }
+    else if (preference === "male" && agerange >= 21 && agerange < 41) {
+        $("#yourdate").addClass("ryangosling");
+        $("#date").text("Ryan Gosling");
+    }
+    else if (preference === "male" && agerange >= 41) {
+        $("#yourdate").addClass("georgeclooney");
+        $("#date").text("George Clooney");
+    }
+    else if (preference === "female" && agerange >= 21 && agerange < 41) {
+        $("#yourdate").addClass("emmastone");
+        $("#date").text("Emma Stone");
+    }
+    else if (preference === "female" && agerange >= 41) {
+        $("#yourdate").addClass("helenmirren");
+        $("#date").text("Helen Mirren");
+    }
+
+  });
+});
